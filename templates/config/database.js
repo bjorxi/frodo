cf9fdff0d1f2104c592170a83a6ab05bfabcb7cd
Nodari Lipartiya <nodari.lipartiya@gmail.com>
2015-11-19 00:09:24 +0300

    + new templates

---
+++ templates/config/database.js
@@ -0,0 +1,11 @@
+/* Database configuration file */
+
+module.exports = {
+  development: {
+    servers: [['127.0.0.1', 27017]],
+    database: '',
+    user: '',
+    password: '',
+    replicaSet: null,
+  }
+}
